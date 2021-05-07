7254b8d52f6bbbfea02d542a4440dfa87085e259
Alisha Saleh <alishasaleh@hotmail.co.uk>
2021-05-07 13:00:34 +0100

    Showing table on page

--- src/App.js
+++ src/App.js
@@ -3,12 +3,15 @@ import './App.css';
 //import Jumbotron from './components/Jumbotron'
 import Jumbotron from 'react-bootstrap/Jumbotron'
 import SearchForm from './components/SearchForm';
+import Container from './components/Container';
+import { Col, Row } from 'react-bootstrap';
+import TableContainer from './components/Table';
 
 function App() {
   const jumboStyle = {
     color: "#FFFFFF",
     backgroundColor: "#6495ed",
-    textAlign: "center",
+    //textAlign: "center",
     fontFamily: "Arial"
   };
   return (
@@ -17,6 +20,14 @@ function App() {
         <h1>Employee Directory</h1>
         <SearchForm />
       </Jumbotron>
+      <Container>
+        <Row>
+          <Col>
+            <TableContainer />
+          </Col>
+        </Row>
+      </Container>
+
     </>
   );
 }
