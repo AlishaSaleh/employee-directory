b2457eb1fc7911f98810651bb5cc1cb27c122dab
Alisha Saleh <alishasaleh@hotmail.co.uk>
2021-05-07 15:44:20 +0100

    UseState and order function

--- src/components/Table/index.js
+++ src/components/Table/index.js
@@ -2,34 +2,26 @@ import React from "react";
 import { Table } from "react-bootstrap";
 
 function TableContainer() {
+   
     return (
         <>
             <Table striped bordered hover>
                 <thead>
                     <tr>
-                        <th>#</th>
-                        <th>First Name</th>
-                        <th>Last Name</th>
-                        <th>Username</th>
+                        <th>Image</th>
+                        <th>Name</th>
+                        <th>Phone</th>
+                        <th>Email</th>
+                        <th>DOB</th>
                     </tr>
                 </thead>
                 <tbody>
                     <tr>
                         <td>1</td>
                         <td>Mark</td>
-                        <td>Otto</td>
-                        <td>@mdo</td>
-                    </tr>
-                    <tr>
-                        <td>2</td>
-                        <td>Jacob</td>
-                        <td>Thornton</td>
-                        <td>@fat</td>
-                    </tr>
-                    <tr>
-                        <td>3</td>
-                        <td colSpan="2">Larry the Bird</td>
-                        <td>@twitter</td>
+                        <td>90230324</td>
+                        <td>mark@mdo.com</td>
+                        <td>01/01/1980</td>
                     </tr>
                 </tbody>
             </Table>
