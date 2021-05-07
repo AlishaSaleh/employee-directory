9e7ea37c5a9807809c99ebd21b7d1cd71da1504a
Alisha Saleh <alishasaleh@hotmail.co.uk>
2021-05-07 15:14:22 +0100

    API updated with parameters

--- src/utils/API.js
+++ src/utils/API.js
@@ -3,8 +3,7 @@ import axios from "axios";
 export default {
     getEmployees: function (query) {
         return axios.get(
-            "https://randomuser.me/api/" +
-            query
+            "https://randomuser.me/api/?results=100&?nat=gb"
         );
     }
 };
